82f3d511f35bf81b99205c8f62ed37ca8ea6ee49
mr008 <wdsong@ucdavis.edu>
2023-10-22 00:05:51 -0400

    Fixed the backgroud video

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -7,24 +7,26 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>Next Gen Physical Therapy</h1>
-      <p>Technology makes healthcare more accessible</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+      <div className='hero-content'>
+        <h1>The Future of Wellness</h1>
+        <p>Technology makes healthcare more accessible</p>
+        <div className='hero-btns'>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+          >
+            GET STARTED
+          </Button>
+          <Button
+            className='btns'
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'
+            onClick={console.log('hey')}
+          >
+            WATCH TRAILER <i className='far fa-play-circle' />
+          </Button>
+        </div>
       </div>
     </div>
   );
